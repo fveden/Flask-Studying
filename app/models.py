ed08f4879c5e95a9ef6401b2c8a8d66e240ceb25
fveden <fedorv1234@gmail.com>
2022-09-12 22:23:46 +0300

    Adding posts table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from tkinter.tix import COLUMN
+from datetime import datetime
 from app import db
 
 
@@ -10,3 +11,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<Post {self.body}>'
