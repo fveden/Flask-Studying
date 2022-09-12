38986d99e332726fa1962737af3e2b44b2340014
fveden <fedorv1234@gmail.com>
2022-09-12 20:31:41 +0300

    Done chapter 3

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from turtle import title
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -28,3 +28,12 @@ def index():
     user = {'username' : 'Fedor'}
 
     return render_template('index.html', title = "Studying Flask", user = user, posts = posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login for user {}, remember_me {}'.format(form.username.data, form.remember_me.data))
+        return redirect( url_for('index') )
+    return render_template('login.html', title = 'Sign In', form = form)
