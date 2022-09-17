04b59dc8995a858e94d02f62dbfc04d0cc266993
fveden <fedorv1234@gmail.com>
2022-09-17 20:56:25 +0300

    Done chapter 6

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
+from email.policy import default
+from msilib.schema import Class
 from tkinter.tix import COLUMN
 from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
+
+
 
 @login.user_loader
 def load_user(id):
@@ -16,6 +21,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default = datetime.utcnow)
 
     def __repr__(self):
         return f"<User {self.username}>"
@@ -25,7 +32,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
     
 
@@ -37,3 +47,5 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.body}>'
+
+
